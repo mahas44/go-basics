a3287812f33668c50554802666ef78bdcb941e3f
mahas44 <enescakmak44@gmail.com>
2023-06-27 15:58:22 +0300

    error handling 1

---
+++ error-handling/check-error.go
@@ -0,0 +1,79 @@
+package errorhandling
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func Check() {
+	//one()
+	// two()
+	three()
+	four()
+}
+
+func one() {
+	n, err := fmt.Println("Hello")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(n)
+}
+
+func two() {
+	var answer1, answer2, answer3 string
+
+	fmt.Print("Name: ")
+	_, err := fmt.Scan(&answer1)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Fav Food: ")
+	_, err = fmt.Scan(&answer2)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Fav Sport: ")
+	_, err = fmt.Scan(&answer3)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(answer1, answer2, answer3)
+}
+
+func three() {
+	f, err := os.Create("./error-handling/names.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer f.Close()
+
+	r := strings.NewReader("James Bond")
+	io.Copy(f, r)
+}
+
+func four() {
+	f, err := os.Open("./error-handling/names.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer f.Close()
+
+	bs, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(bs))
+}
--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	// jsn "go-basics/jsnbasic"
 	// conc "go-basics/concurrency"
 	// chn "go-basics/channels"
-	lvl10 "go-basics/exercises/level10"
+	errorhandling "go-basics/error-handling"
+	// lvl10 "go-basics/exercises/level10"
 )
 
 func main() {
@@ -16,5 +17,6 @@ func main() {
 	// chn.Fanin()
 	// chn.Fanout()
 	// chn.Context()
-	lvl10.Exercise7()
+	// lvl10.Exercise7()
+	errorhandling.Check()
 }
