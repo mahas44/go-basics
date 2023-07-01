5ffbf1e0c7a3662573db3715ecc6e505bb2cff6d
mahas44 <enescakmak44@gmail.com>
2023-07-01 17:18:53 +0300

    recover

---
+++ error-handling/recover.go
@@ -0,0 +1,31 @@
+package errorhandling
+
+import "fmt"
+
+func Recover() {
+	f()
+	fmt.Println("Returned normaly from f.")
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normaly from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking")
+		panic(fmt.Sprintf("%v", i))
+	}
+
+	defer fmt.Println("Defer in g.", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
