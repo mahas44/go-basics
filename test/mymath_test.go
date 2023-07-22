8634c60e24d7fece6e2ef22306498e1f4527209a
mahas44 <enescakmak44@gmail.com>
2023-07-22 23:39:25 +0300

    tabel test

--- test/mymath_test.go
+++ test/mymath_test.go
@@ -3,9 +3,24 @@ package test
 import "testing"
 
 func TestMySum(t *testing.T) {
-	x := MySum(2, 3)
-	if x != 5 {
-		t.Error("Expected", 5, "Got", x)
+
+	type test struct {
+		data   []int
+		answer int
+	}
+
+	tests := []test{
+		{[]int{21, 21}, 42},
+		{[]int{3, 4, 5}, 12},
+		{[]int{1, 1}, 2},
+		{[]int{-1, 0, 1}, 0},
+	}
+
+	for _, v := range tests {
+		x := MySum(v.data...)
+		if x != v.answer {
+			t.Error("Expected", v.answer, "Got", x)
+		}
 	}
 }
 
